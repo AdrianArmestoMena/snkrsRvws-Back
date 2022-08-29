a83f60ae2845b2fcf7ddf15ffe92a9340e24bd49
Adrian Armesto <armestomena@gmail.com>
2022-08-29 16:22:55 +0200

    Created start server function

---
+++ src/server/startServer.ts
@@ -0,0 +1,23 @@
+import "../loadEnvironment";
+import Debug from "debug";
+import chalk from "chalk";
+import express from "express";
+import cors from "cors";
+
+const debug = Debug("sneakersReview:index");
+
+export const app = express();
+app.use(cors());
+
+export const startServer = (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.blue("Server listening"));
+      resolve(true);
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.yellow("Error connecting to database", error.message));
+      reject(error);
+    });
+  });
