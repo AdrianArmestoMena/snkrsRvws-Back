94b60509b3cb06baf110b92ce4ba812e52745c4d
Adrian Armesto <armestomena@gmail.com>
2022-08-30 17:01:55 +0200

    Change uesr controller test

--- src/controllers/usersControllers.test.ts
+++ src/controllers/usersControllers.test.ts
@@ -2,18 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../database/models/users";
 import signUp from "./usersControllers";
 
-const mockVerifyResolution = {};
-const hashCompareReturn: boolean | Error = true;
-
 const next = jest.fn() as Partial<NextFunction>;
-
-jest.mock("../utils/auth", () => ({
-  ...jest.requireActual("../utils/auth"),
-  hashCreator: () => jest.fn().mockReturnValue(""),
-  verifyToken: () => jest.fn().mockReturnValue(mockVerifyResolution),
-  createToken: jest.fn().mockReturnValue(""),
-  hashCompare: () => jest.fn().mockReturnValue(hashCompareReturn),
-}));
 
 describe("Given a sign up controller", () => {
   const mockedReqBody = {
