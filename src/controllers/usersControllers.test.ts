0189cf15978d8de73ad0d14bc0b0c1f2d1dc60e6
Adrian Armesto <armestomena@gmail.com>
2022-08-31 15:15:59 +0200

    Test log in controller

--- src/controllers/usersControllers.test.ts
+++ src/controllers/usersControllers.test.ts
@@ -1,14 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../database/models/users";
-import signUp from "./usersControllers";
+import { Payload } from "../types/payload";
+import { createToken } from "../utils/auth";
+import { logIn, signUp } from "./usersControllers";
 
 const next = jest.fn() as Partial<NextFunction>;
 
+const hashCompareValue = false;
+
+jest.mock("../utils/auth", () => ({
+  ...jest.requireActual("../utils/auth"),
+  hashCompare: () => jest.fn().mockReturnValue(hashCompareValue),
+  createToken: jest.fn().mockReturnValue(""),
+}));
+
 describe("Given a sign up controller", () => {
   const mockedReqBody = {
-    userName: "assddsdsdsdsd",
-    password: "asdghf",
-    email: "stringas",
+    userName: "Adrian",
+    password: "AdrianArm",
+    email: "adrian@adrian",
   };
 
   const req = {
@@ -57,6 +67,121 @@ describe("Given a sign up controller", () => {
       } as Partial<Request>;
 
       await signUp(errorReq as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a log in controller", () => {
+  afterEach(() => jest.restoreAllMocks());
+
+  const mockedReqBody = {
+    userName: "Adrian",
+    password: "AdrianArm",
+  };
+
+  const req = {
+    body: mockedReqBody,
+  } as Partial<Request>;
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as Partial<Response>;
+
+  User.find = jest.fn().mockReturnValue([mockedReqBody]);
+
+  describe("When it is called with a Request a Response and a Next fucntion", () => {
+    test("Then it should call the status method of the response", async () => {
+      await logIn(req as Request, res as Response, next as NextFunction);
+
+      const statusCode = 200;
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+
+    test("Then it should call the json method of the response", async () => {
+      await logIn(req as Request, res as Response, next as NextFunction);
+
+      const payLoad: Payload = {
+        id: "123",
+        userName: "asdfghj",
+      };
+
+      const responseData = {
+        user: { token: createToken(payLoad) },
+      };
+
+      expect(res.json).toHaveBeenCalledWith(responseData);
+    });
+
+    test("It should call the next function with the created error if the request body does not fulfill contract", async () => {
+      mockedReqBody.userName = "a";
+      mockedReqBody.password = "a";
+
+      await logIn(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    test("It should call the next function with the created error if it wasn't posible to find the user", async () => {
+      mockedReqBody.userName = "Adrian";
+      mockedReqBody.password = "AdrianArm";
+
+      User.find = jest.fn().mockReturnValue([]);
+
+      await logIn(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    test("It should call the next function with the created error if an user find throw an error", async () => {
+      mockedReqBody.userName = "Adrian";
+      mockedReqBody.userName = "AdrianArm";
+
+      User.find = jest.fn().mockRejectedValue(new Error());
+
+      await logIn(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a log in controller", () => {
+  afterEach(() => jest.restoreAllMocks());
+
+  jest.mock("../utils/auth", () => ({
+    ...jest.requireActual("../utils/auth"),
+    hashCompare: () => jest.fn().mockReturnValue(false),
+    createToken: jest.fn().mockReturnValue(""),
+  }));
+
+  const mockedReqBody = {
+    userName: "Adrian",
+    password: "AdrianArm",
+  };
+
+  const req = {
+    body: mockedReqBody,
+  } as Partial<Request>;
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as Partial<Response>;
+
+  User.find = jest.fn().mockReturnValue([mockedReqBody]);
+
+  describe("When it is called with a Request a Response and a Next fucntion", () => {
+    test("It should call the next function with the created error if an user find throw an edfdsfdrror", async () => {
+      mockedReqBody.userName = "Adrian";
+      mockedReqBody.userName = "AdrianArm";
+
+      jest.resetAllMocks();
+
+      await logIn(req as Request, res as Response, next as NextFunction);
 
       expect(next).toHaveBeenCalled();
     });
