cf84aa1bfe62395b1fae98bd912a10437fae4de8
Adrian Armesto <armestomena@gmail.com>
2022-08-31 15:15:17 +0200

    Created login controller

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../database/models/users";
 import signUpSchema from "../schemas/signUpSchema";
-import { ReqUser } from "../types/user";
-import { hashCreator } from "../utils/auth";
+import { ReqUser, UserWithId } from "../types/user";
+import { createToken, hashCompare, hashCreator } from "../utils/auth";
 import createCustomError from "../utils/error";
+import { Payload } from "../types/payload";
+import logInSchema from "../schemas/logInChema";
 
-const signUp = async (req: Request, res: Response, next: NextFunction) => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const user: ReqUser = req.body;
 
   try {
@@ -38,4 +44,81 @@ const signUp = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default signUp;
+export const logIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.body as { userName: string; password: string };
+  const errorCustom = createCustomError(
+    400,
+    "Authentication error",
+    "Authentication error"
+  );
+
+  let findUser: Array<UserWithId>;
+
+  try {
+    const validation = logInSchema.validate(user, {
+      abortEarly: false,
+    });
+
+    if (validation.error) {
+      throw new Error(Object.values(validation.error.message).join(""));
+    }
+  } catch (error) {
+    next(errorCustom);
+    return;
+  }
+
+  try {
+    findUser = await User.find({
+      userName: user.userName,
+      pasword: user.password,
+    });
+
+    if (!findUser.length) {
+      next(errorCustom);
+      return;
+    }
+  } catch (error) {
+    const customError = createCustomError(
+      400,
+      (error as Error).message,
+      "Authentication error"
+    );
+    next(customError);
+    return;
+  }
+
+  try {
+    const isPaswordValid = await hashCompare(
+      user.password,
+      findUser[0].password
+    );
+
+    if (!isPaswordValid) {
+      throw new Error();
+    }
+  } catch (error) {
+    const customError = createCustomError(
+      400,
+      (error as Error).message,
+      "Authentification error"
+    );
+
+    next(customError);
+    return;
+  }
+
+  const userFinal: Payload = {
+    userName: findUser[0].userName,
+    id: findUser[0].id,
+  };
+
+  const responseData = {
+    user: { token: createToken(userFinal) },
+  };
+
+  res.status(200).json(responseData);
+};
