9f2e5afdb5beef0e1b2b09267a508eef9d8b74dd
Adrian Armesto <armestomena@gmail.com>
2022-09-01 09:51:11 +0200

    Add joi validation on router and error middleware

--- src/schemas/logInChema.ts
+++ src/schemas/logInChema.ts
@@ -1,8 +1,10 @@
 import { Joi } from "express-validation";
 
-const logInSchema = Joi.object({
-  userName: Joi.string().min(3).max(15).required(),
-  password: Joi.string().min(5).required(),
-});
+const logInSchema = {
+  body: Joi.object({
+    userName: Joi.string().min(3).max(15).required(),
+    password: Joi.string().min(5).required(),
+  }),
+};
 
 export default logInSchema;
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,9 +1,11 @@
 import { Joi } from "express-validation";
 
-const signUpSchema = Joi.object({
-  userName: Joi.string().min(3).max(15).required(),
-  password: Joi.string().min(5).required(),
-  email: Joi.string().min(7).required(),
-});
+const signUpSchema = {
+  body: Joi.object({
+    userName: Joi.string().min(3).max(15).required(),
+    password: Joi.string().min(5).required(),
+    email: Joi.string().min(7).required(),
+  }),
+};
 
 export default signUpSchema;
