d0dfee7bdd71bb63812b28f9ad5da83ef2dff112
Adrian Armesto <armestomena@gmail.com>
2022-09-05 12:28:47 +0200

    Created get owners review controller

--- src/server/routers/reviewsRouter.ts
+++ src/server/routers/reviewsRouter.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import multer from "multer";
-import createReview from "../../controllers/reviews/reviewsController";
+import {
+  createReview,
+  getOwnerReviews,
+} from "../../controllers/reviews/reviewsController";
 
 const reviewsRouter = express.Router();
 const upload = multer({ dest: "uploads", limits: { fileSize: 3000000 } });
 
 reviewsRouter.post("/addreview", upload.single("picture"), createReview);
+reviewsRouter.get("/:owner", getOwnerReviews);
 export default reviewsRouter;
