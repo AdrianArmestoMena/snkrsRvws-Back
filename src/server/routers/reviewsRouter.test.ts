0b82147cb9564bfed17e35712808072ece36e968
Adrian Armesto <armestomena@gmail.com>
2022-09-07 18:44:54 +0200

    Tested delete route

--- src/server/routers/reviewsRouter.test.ts
+++ src/server/routers/reviewsRouter.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import connectDB from "../../database";
 import app from "..";
 import { User } from "../../database/models/users";
+import { Review } from "../../database/models/reviews";
 
 let mongoServer: MongoMemoryServer;
 
@@ -101,6 +102,51 @@ describe("Given a /reviews/:owner route", () => {
       const expectedStatus = 400;
 
       await request(app).get(`/reviews/${mockUser.id}`).expect(expectedStatus);
+    });
+  });
+});
+
+describe("Given a /reviews/:idReview route", () => {
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IjEyMzQ1NjciLCJpZCI6IjYzMTBkMTQyNjEyYjFmMGExY2VjODk2MSIsImlhdCI6MTY2MjM4NzIwOH0.5FBUtcB9yv6R2W-lFk9H14dmAOOecruuDAX6LVwPwiQ";
+  describe("When it is requested with a delete method and a id as param", () => {
+    test("Then it should respond with a status of 201", async () => {
+      const userReview = {
+        brand: "NIke",
+        model: "Jordan 11 low black and white",
+        picture: "02a4502cd2a7de527d98a8c3e7871891",
+        review: "dwjvniu jwdwg r3jgn3r g3r gj rtgkj4",
+        owner: "6310d142612b1f0a1cec8961",
+        id: "631624344805e6655b5b144a",
+      };
+
+      const mockReview = await Review.create(userReview);
+
+      const expectedStatus = 201;
+
+      await request(app)
+        .delete(`/reviews/${mockReview.id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(expectedStatus);
+    });
+
+    test("Then it should respond with a status of 400 if some value is missing", async () => {
+      const reviewMock = {
+        brand: "NIke",
+        model: "Jordan 11 low black and white",
+        picture: "02a4502cd2a7de527d98a8c3e7871891",
+        review: "dwjvniu jwdwg r3jgn3r g3r gj rtgkj4",
+        owner: "6310d142612b1f0a1cec8961",
+        id: "631624344805e6655b5b144a",
+      };
+      const incorrectId = 1;
+      await Review.create(reviewMock);
+      const expectedStatus = 404;
+
+      await request(app)
+        .delete(`/reviews/${incorrectId}}`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect(expectedStatus);
     });
   });
 });
