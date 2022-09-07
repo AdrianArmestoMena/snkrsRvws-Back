452c00f7eeebe0a52fc0715aacf2c5a1c5b79537
Adrian Armesto <armestomena@gmail.com>
2022-09-07 09:54:59 +0200

    Added cors options

---
+++ src/utils/corsOptions.ts
@@ -0,0 +1,20 @@
+interface IcorsOptions {
+  origin: string[];
+}
+
+const corsOptions: IcorsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:4000",
+    "http://localhost:4001",
+    "http://localhost:4002",
+    "http://localhost:5000",
+    "http://localhost:5001",
+    "http://localhost:5002",
+    "https://adrian-armesto-front-final-project.netlify.app/",
+  ],
+};
+
+export default corsOptions;
