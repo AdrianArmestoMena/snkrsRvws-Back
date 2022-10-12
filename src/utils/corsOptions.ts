cb15390cffcc809cbc252be884b49d43d22ee781
Adrian Armesto <armestomena@gmail.com>
2022-10-12 20:48:35 +0200

    Changed cors options beacuse railway

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -13,8 +13,8 @@ const corsOptions: IcorsOptions = {
     "http://localhost:5000",
     "http://localhost:5001",
     "http://localhost:5002",
-    "https://adrian-armesto-front-final-project.netlify.app/",
-    "https://adrian-armesto-front-final-project.netlify.app",
+    "https://snkrsrvws-back-production.up.railway.app/reviews/",
+    "https://snkrsrvws-back-production.up.railway.app/reviews",
   ],
 };
 
